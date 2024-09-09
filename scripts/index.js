9c0de74f4fc356edb4258ebb78f632ea665e2419
Kirill Churakov <multiair345@yandex.ru>
2024-09-09 18:52:14 +0400

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const cardsList = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
 
 // @todo: Функция создания карточки
 
+function cardAdd(title, link) {
+    const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardTitle = cardItem.querySelector('.card__title');
+    const cardImage = cardItem.querySelector('.card__image');
+    cardTitle.textContent = title;
+    cardImage.setAttribute('src', link);
+    cardImage.setAttribute('alt', title);
+    cardItem.querySelector('.card__delete-button').addEventListener('click', () => {
+        cardDelete(cardItem);
+    });
+    return cardItem;
+}
+
 // @todo: Функция удаления карточки
 
+function cardDelete(cardItem) {
+    cardItem.remove()
+};
+
 // @todo: Вывести карточки на страницу
+
+function cardCreate(container, cardData) {
+    container.append(cardData);
+};
+
+initialCards.forEach(function(object) {
+    cardCreate(cardsList, cardAdd(object.name, object.link))
+});
+
