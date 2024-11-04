9ae8a42a39f443507fd5e33f59e9b0f93257e493
Kirill Churakov <multiair345@yandex.ru>
2024-11-04 21:27:44 +0400

    1

--- src/index.js
+++ src/index.js
@@ -44,16 +44,16 @@ import './index.css';
 
 const popup = document.querySelector('.popup');
 const profileEdit = document.querySelector('.profile__edit-button');
-const popupClosed = document.querySelector('.popup__close');
+const popupClosed = document.querySelectorAll('.popup__close');
 const button = document.querySelector('.button');
 const popupEdit = document.querySelector('.popup_type_edit');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputDescription = document.querySelector('.popup__input_type_description');
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
+const addCard = document.querySelector('.popup_type_new-card');
 
-
-const editForm = document.querySelector('.popup__form[name="edit-profile]');
+const editForm = document.querySelector('.popup__form[name="edit-profile"]');
 
 
 
@@ -62,25 +62,37 @@ const editForm = document.querySelector('.popup__form[name="edit-profile]');
 
 function popupOpen(popup) {
     popup.classList.add('popup_is-opened');
-}
+};
 function popupClose(popup) {
     popup.classList.remove('popup_is-opened');
-}
+};
 
+
+addButton.addEventListener('click', function() {
+    popupOpen(addCard);
+});
 
 
 profileEdit.addEventListener('click', function(e) {
     popupOpen(popupEdit);
     inputName.value = profileTitle.textContent;
     inputDescription.value = profileDescription.textContent;
-})
+});
 
-popupClosed.addEventListener('click', function(e) {
-    popupClose(popupClosed.closest('.popup_is-opened'))
-})
 
-document.addEventListener('keydown', function(evt) {
-    if (evt.code === "Escape" && popup.classList.contains('popup_is-opened')) {
+popupClosed.forEach(button => {
+    button.addEventListener('click', function() {
+        const popup = button.closest('.popup');
         popupClose(popup);
+    });
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.code === "Escape") {
+        const openPopups = document.querySelectorAll('.popup_is-opened');
+        openPopups.forEach(popup => {
+            popupClose(popup);
+        });
     }
-})
+});
+
