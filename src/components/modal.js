6c930d2f2ce8132c33551fdf4b42fac99f03d137
Kirill Churakov <multiair345@yandex.ru>
2024-11-11 19:25:39 +0400

    правки

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,13 +1,6 @@
 const closePopupButtons = document.querySelectorAll('.popup__close');
 const popup = document.querySelectorAll('.popup');
 
-export function handleProfileFormSubmit(e) {
-    e.preventDefault();
-    profileTitle.textContent = inputName.value;
-    profileDescription.textContent = inputDescription.value;
-    closePopup(popupEdit);
-};
-
 export function openPopup(popup) {
     popup.classList.add('popup_is-opened');
     document.addEventListener('keydown', closePopupByEsc);
@@ -18,22 +11,18 @@ export function closePopup(popup) {
     document.removeEventListener('keydown', closePopupByEsc)
 };
 
-export function closedPopup() {
-    closePopupButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const popup = button.closest('.popup');
-            closePopup(popup);
-        });
+closePopupButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const popup = button.closest('.popup');
+        closePopup(popup);
     });
-};
+});
 
-export function closedPopupOverlay () {
-    popup.forEach(popup => {
-        popup.addEventListener('click', function(e) {
-         closePopup(e.target);
-        })
-     })
-};
+popup.forEach(popup => {
+    popup.addEventListener('click', function(e) {
+     closePopup(e.target);
+    })
+ })
 
 export function closePopupByEsc(e) {
     if(e.key === 'Escape') {
--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
-import {initialCards} from './cards.js'
-import './index.css';
+import { initialCards } from './cards.js'
 import { createCardNode, deleteCard, likeButtonClicked } from './components/card.js';
-import { handleProfileFormSubmit, closePopup, openPopup, closedPopup, closedPopupOverlay, closePopupByEsc } from './components/modal.js';
+import { closePopup, openPopup } from './components/modal.js';
+import './index.css';
 
 const cardsList = document.querySelector('.places__list');
 const addButton = document.querySelector('.profile__add-button');
-const popup = document.querySelectorAll('.popup');
 const profileEdit = document.querySelector('.profile__edit-button');
 const popupEdit = document.querySelector('.popup_type_edit');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputDescription = document.querySelector('.popup__input_type_description');
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
-const addCard = document.querySelector('.popup_type_new-card');
-const inputCardName = document.querySelector('.popup__input_type_card-name');
-const inputCardUrl = document.querySelector('.popup__input_type_url');
+const popupNewCard = document.querySelector('.popup_type_new-card');
 const popupImage = document.querySelector('.popup_type_image');
-const openImage = document.querySelector('.popup__image');
-const popupTitle = document.querySelector('.popup__caption');
-
-closedPopup();
-closedPopupOverlay();
 
 addButton.addEventListener('click', function() {
-    openPopup(addCard);
+    openPopup(popupNewCard);
 });
 
-addCard.addEventListener('submit', (e) => {
+popupNewCard.addEventListener('submit', (e) => {
+    const inputCardName = popupNewCard.querySelector('.popup__input_type_card-name');
+    const inputCardUrl = popupNewCard.querySelector('.popup__input_type_url');
     e.preventDefault();
     const card = createCardNode(inputCardName.value, inputCardUrl.value, likeButtonClicked, openPopupImage);
     prependCardToPage(cardsList, card);
-    closePopup(addCard);
+    closePopup(popupNewCard);
     inputCardName.value = '';
     inputCardUrl.value = '';
 });
@@ -41,11 +35,11 @@ initialCards.forEach(function(object) {
 
 function appendCardToPage(container, cardData) {
     container.append(cardData);
-};
+}
 
 function prependCardToPage(container, cardData) {
     container.prepend(cardData);
-};
+}
 
 profileEdit.addEventListener('click', function(e) {
     openPopup(popupEdit);
@@ -56,10 +50,18 @@ profileEdit.addEventListener('click', function(e) {
 popupEdit.addEventListener('submit', handleProfileFormSubmit);
 
 function openPopupImage(title, link) {
+    const openImage = popupImage.querySelector('.popup__image');
+    const popupTitle = popupImage.querySelector('.popup__caption');
     openImage.src = link;
     openImage.alt = title;
     popupTitle.textContent = title;
 
     openPopup(popupImage);
-};
+}
 
+function handleProfileFormSubmit(e) {
+    e.preventDefault();
+    profileTitle.textContent = inputName.value;
+    profileDescription.textContent = inputDescription.value;
+    closePopup(popupEdit);
+}
